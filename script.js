51f05e1d47b92f1764eab65e1350b6b776097d74
VUPPU KULADEEP <vuppukuladeep@gmail.com>
2025-07-01 18:52:58 +0530

    completed regex

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
 function check(event){
-    console.log('entered')
     event.preventDefault()
     const name = document.querySelector('#name').value
     const email = document.querySelector('#email').value
     const message = document.querySelector('#message').value
     
-    if (!name.match(/^[a-zA-Z]{6,10}$/)){
-        console.log('error occurred')
+    if(!name || !email || !message){
+        alert('fill all the details first')
+        return
     }
-    
+
+    if (!name.match(/^[a-zA-Z]{6,10}$/)){
+        alert('name invalid')
+        return
+    }
+    if (!email.match(/^[a-z0-9]+@[a-z]+\.[a-z]+$/)){
+        alert('invalid email')
+        return
+    }
+
+    if (message.match(/[.]/)){
+        alert('message is not valid')
+        return
+    }
+    alert('submitted')
+    return
 }
