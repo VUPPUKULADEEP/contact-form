2fd8a23ab357a9a3f87ddde3bb2cb95490f2614b
VUPPU KULADEEP <vuppukuladeep@gmail.com>
2025-07-01 19:42:18 +0530

    added styles

--- script.js
+++ script.js
@@ -1,27 +1,41 @@
+
+
 function check(event){
     event.preventDefault()
     const name = document.querySelector('#name').value
     const email = document.querySelector('#email').value
     const message = document.querySelector('#message').value
+    const alert = document.querySelector('.alert')
     
+    alert.style.color = 'red'
+
     if(!name || !email || !message){
-        alert('fill all the details first')
-        return
+        alert.textContent = 'fill all the details first'
+        
     }
 
-    if (!name.match(/^[a-zA-Z]{6,10}$/)){
-        alert('name invalid')
-        return
+    else if (!name.match(/^[a-zA-Z]{6,10}$/)){
+        alert.textContent = 'name invalid'
+        
     }
-    if (!email.match(/^[a-z0-9]+@[a-z]+\.[a-z]+$/)){
-        alert('invalid email')
-        return
+    else if (!email.match(/^[a-z0-9]+@[a-z]+\.[a-z]+$/)){
+        alert.textContent = 'invalid email'
+        
     }
 
-    if (message.match(/[.]/)){
-        alert('message is not valid')
-        return
+    else if (message.match(/[.]/)){
+        alert.textContent = 'message is not valid'
+        
     }
-    alert('submitted')
+    else{
+            alert.style.color = 'green'
+    alert.textContent = 'submitted'
+    }
+
+
+    setTimeout(()=>{
+    alert.textContent = null
+    },3000)
     return
 }
+
